01348201c08035826d8905b4f2d7ff5634e838d4
jamesleu923 <jamesleu923@gmail.com>
2024-11-21 11:41:20 +0800

    上課時講解時,修正

--- CS_Fun01/Program.cs
+++ CS_Fun01/Program.cs
@@ -12,16 +12,35 @@ namespace CS_Fun01
         const double pi = 3.14159265359;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
-            Console.WriteLine("h!");
-            countsum04(5);
+            Console.WriteLine("Call Countsum02:{0:N5}", countsum02());
+            Console.WriteLine("Call Countsum03:當半徑為3時,面積為:{0:N5}", countsum03());
+            countsum04(4);
+            double z=countsum06(3.14f, 5, 'a');
+            Console.WriteLine("Countsum06回傳Z值為:{0:N5}", z);
+            Console.ReadLine();
         }
 
         //回傳值型態不同,無傳入值的函式 樣態
         static void countsum01()
         {
             // void 不用return回傳, 也可以打 return; 結束,代表沒有回傳值
-            return;
+            int k = 1, flag = 0;
+            do
+            {
+                k = k + 3;
+                if (k % 3 == 0)
+                {
+                    flag = 1;
+                    break;
+                }                    
+            } while (k < 100000);
+
+            if (flag == 1)  //找到3的倍數
+            {
+                //做我們的程式...
+            }
+            else
+                return;  //用return; 強制結束函式
         }
         static int countsum02()
         {
@@ -44,10 +63,10 @@ namespace CS_Fun01
             // d:十進位數, g:一般顯示, n:小數點
             Console.WriteLine("countsum04 你的圓半徑為:{0:d3},直徑為{1:d3},面積為:{2:n2}", a, 2 * a, pi*a*a);
         }
-        static int countsum05(float b)
+        static double countsum05(float b)
         {
-
-            return (int)(2 * b);
+            //傳入半徑回傳直徑;
+            return (2 * b);
         }
         static double countsum06(float a, int b, char k)
         {
